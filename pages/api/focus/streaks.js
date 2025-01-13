46d39d1b2ac493d9e119e83198efeb3205d5f212
Probir Ghosh <probirghosh@outlook.com>
2025-01-13 21:44:13 +0600

    fixing deploying issue :hammer:

--- pages/api/focus/streaks.js
+++ pages/api/focus/streaks.js
@@ -1,4 +1,4 @@
-import dbConnect from '../../../../utils/dbConnect';
+import dbConnect from '../../../utils/dbconnect';
 import FocusSession from '../../../models/FocusSession';
 import { authenticateToken } from '../../../utils/auth';
 
